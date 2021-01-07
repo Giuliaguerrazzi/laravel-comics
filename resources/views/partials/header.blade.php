939d6d67b15862d0178412e9b6ffdd091be98d6b
Giulia <giulia.guerrazzi@live.it>
2021-01-07 15:47:17 +0100

    add logo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,9 @@
+<header class='main-header'>
+    <div class='container'>
+        <a class='navbar-brand' href="">
+            <img src="{{ asset('images/logo.png') }}" alt="">
+        </a>
+    </div>
+
+
+</header>
